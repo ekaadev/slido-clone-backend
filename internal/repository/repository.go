ad904944a1e35bcb761089bdf7c1c43e5c4d8a80
ekaadev <ekaadev9@gmail.com>
2025-11-16 21:04:07 +0700

    Add generic repository and user repository implementations

---
+++ internal/repository/repository.go
@@ -0,0 +1,32 @@
+package repository
+
+import "gorm.io/gorm"
+
+// Repository melakukan operasi database pada database
+// Repository menggunakan data entity untuk melakukan operasi database
+type Repository[T any] struct {
+	DB *gorm.DB
+}
+
+// Create melakukan operasi insert pada entity
+func (r *Repository[T]) Create(db *gorm.DB, entity *T) error {
+	return db.Create(entity).Error
+}
+
+func (r *Repository[T]) Update(db *gorm.DB, entity *T) error {
+	return db.Save(entity).Error
+}
+
+func (r *Repository[T]) Delete(db *gorm.DB, entity *T) error {
+	return db.Delete(entity).Error
+}
+
+func (r *Repository[T]) CountById(db *gorm.DB, id any) (int64, error) {
+	var count int64
+	err := db.Model(new(T)).Where("id = ?", id).Count(&count).Error
+	return count, err
+}
+
+func (r *Repository[T]) FindById(db *gorm.DB, entity *T, id any) error {
+	return db.Where("id = ?", id).Take(entity).Error
+}
---
+++ internal/repository/user_repository.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"errors"
+	"slido-clone-backend/internal/entity"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	Repository[entity.User]
+	Log *logrus.Logger
+}
+
+// NewUserRepository create new instance of UserRepository
+func NewUserRepository(log *logrus.Logger) *UserRepository {
+	return &UserRepository{
+		Log: log,
+	}
+}
+
+// FindByEmailOrUsername more efficient to find user by email or username
+func (r *UserRepository) FindByEmailOrUsername(db *gorm.DB, email string, username string) (*entity.User, error) {
+	var user entity.User
+	err := db.Where("email = ? OR username = ?", email, username).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+
+	return &user, err
+}
