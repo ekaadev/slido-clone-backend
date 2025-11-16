f50d868572ce02c511c8b33853a930b51ffc95a0
ekaadev <ekaadev9@gmail.com>
2025-11-16 21:04:58 +0700

    Add route configuration and guest route for user registration

---
+++ internal/delivery/http/route/route.go
@@ -0,0 +1,23 @@
+package route
+
+import (
+	"slido-clone-backend/internal/delivery/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type RouteConfig struct {
+	App            *fiber.App
+	UserController *http.UserController
+	AuthMiddleware fiber.Handler
+}
+
+// Setup running all route setup here
+func (c *RouteConfig) Setup() {
+	c.SetupGuestRoute()
+}
+
+// SetupGuestRoute tambahkan route yang bisa diakses tanpa autentikasi
+func (c *RouteConfig) SetupGuestRoute() {
+	c.App.Post("/api/v1/users/register", c.UserController.Register)
+}
