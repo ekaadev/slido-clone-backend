b3b442ad7e9be8d2889f2272f3a580d137a2e9d9
ekaadev <ekaadev9@gmail.com>
2025-11-17 22:18:02 +0700

    Add authentication routes and integrate room creation endpoint

--- internal/delivery/http/route/route.go
+++ internal/delivery/http/route/route.go
@@ -9,16 +9,23 @@ import (
 type RouteConfig struct {
 	App            *fiber.App
 	UserController *http.UserController
+	RoomController *http.RoomController
 	AuthMiddleware fiber.Handler
 }
 
 // Setup running all route setup here
 func (c *RouteConfig) Setup() {
 	c.SetupGuestRoute()
+	c.SetupAuthRoute()
 }
 
 // SetupGuestRoute tambahkan route yang bisa diakses tanpa autentikasi
 func (c *RouteConfig) SetupGuestRoute() {
 	c.App.Post("/api/v1/users/register", c.UserController.Register)
 	c.App.Post("/api/v1/users/login", c.UserController.Login)
+}
+
+func (c *RouteConfig) SetupAuthRoute() {
+	c.App.Use(c.AuthMiddleware)
+	c.App.Post("/api/v1/rooms", c.RoomController.Create)
 }
